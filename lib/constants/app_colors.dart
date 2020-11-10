e0e199419da6211f4edf869ef4ac3f383248a537
MSAlbert40 <54085998+MSAlbert40@users.noreply.github.com>
2020-11-10 15:54:31 -0500

    Add libraries and resources to the project
    Libraries were added in the yaml file that will be used in the project, as well as adding an assets folder for the application resources. Finally, 2 dart files were created containing the fonts and colors of the text.

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blue = Color(0xFF0E24DC);
+  static const green = Color(0xFF66FA02);
+  static const cyan = Color(0xFF12CCFD);
+  static const red = Color(0xFFFF1650);
+  static const white = Color(0xFFFFFFFF);
+  static const dark = Color(0xFF000000);
+  static const lead = Color(0xFF8C8C8C);
+  static const gray = Color(0xFFF5F6FA);
+}
---
+++ lib/constants/app_fonts.dart
@@ -0,0 +1,5 @@
+class AppFonts {
+  static const afterNight = 'AfterNight';
+  static const bebasNeue = 'BebasNeue';
+  static const centuryGothic = 'CenturyGothic';
+}
