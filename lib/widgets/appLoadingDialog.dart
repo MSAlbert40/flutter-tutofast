89dc13baebcfb3ab6408ac400a2e950751daffed
MSAlbert40 <54085998+MSAlbert40@users.noreply.github.com>
2020-11-22 10:57:31 -0500

    Update AppLoadingDialog

--- lib/widgets/appLoadingDialog.dart
+++ lib/widgets/appLoadingDialog.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tutofast/constants/app_colors.dart';
+import 'package:flutter_tutofast/constants/app_fonts.dart';
 import 'package:get/get.dart';
 
 class AppLoadingDialog extends StatelessWidget {
@@ -15,112 +16,125 @@ class AppLoadingDialog extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Stack(children: [
-        Container(
-          width: screenWidth,
-          height: screenHeight,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/backgrounds/BT.jpg')
+      body: Center(
+        child: Stack(children: [
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/backgrounds/BT.jpg')
+              )
             )
-          )
-        ),
-        Container(
-          width: screenWidth,
-          height: screenHeight,
-          color: AppColors.blue.withOpacity(0.55)
-        ),
-        Stack(children: [
-          Positioned.fill(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: screenHeight / 1.5,
-                  // color: AppColors.blue,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 200.00,
-                        height: 200.00,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('assets/images/icons/OL.png')
-                          )
-                        )
-                      )
-                    ]
-                  )
-                ),
-                Container(
-                  height: screenHeight / 9,
-                  // color: AppColors.green,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(children: [
+          ),
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            color: AppColors.blue.withOpacity(0.55)
+          ),
+          Stack(children: [
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: screenWidth,
+                    height: screenHeight,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
                         Container(
-                          width: screenWidth / 2,
-                          alignment: Alignment.centerRight,
-                          child: AutoSizeText(
-                            'Tuto',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 65.00,
-                              color: AppColors.white,
-                              fontWeight: FontWeight.bold
-                            )
+                          height: screenHeight / 1.3,
+                          // color: AppColors.red,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 200.0,
+                                height: 200.0,
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage('assets/images/icons/OL.png')
+                                  )
+                                )
+                              )
+                            ]
                           )
                         ),
                         Container(
-                          width: screenWidth / 2,
-                          alignment: Alignment.centerLeft,
-                          child: AutoSizeText(
-                            'Fast',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 65.00,
-                              color: AppColors.cyan,
-                              fontWeight: FontWeight.bold
+                          height: screenHeight / 4.5,
+                          // color: AppColors.green,
+                          child: Column(children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              Container(
+                                width: screenWidth / 2,
+                                alignment: Alignment.centerRight,
+                                child: AutoSizeText(
+                                  'Tuto',
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    color: AppColors.white,
+                                    fontSize: 60,
+                                    fontFamily: AppFonts.centuryGothic,
+                                    fontWeight: FontWeight.bold
+                                  )
+                                ), 
+                              ),
+                              Container(
+                                width: screenWidth / 2,
+                                alignment: Alignment.centerLeft,
+                                child: AutoSizeText(
+                                  'Fast',
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    color: AppColors.cyan,
+                                    fontSize: 60.0,
+                                    fontWeight: FontWeight.bold
+                                  )
+                                ), 
+                              )
+                            ]),
+                            Container(
+                              width: screenWidth,
+                              alignment: Alignment.center,
+                              child: AutoSizeText(
+                                'Copyright © Evertix',
+                                maxLines: 1,
+                                style: TextStyle(
+                                  color: AppColors.white,
+                                  fontSize: 17.0,
+                                  fontWeight: FontWeight.normal
+                                )
+                              )
                             )
-                          )
-                        )
-                      ]),
-                      Container(
-                        width: screenWidth,
-                        child: AutoSizeText(
-                          'Copyright © Evertix',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 17.00,
-                            color: AppColors.white,
-                            fontWeight: FontWeight.normal
-                          )
-                        )
-                      )
-                    ]
+                          ])
+                        ),
+                    ])
                   )
-                )
+              ])
+            )
+          ]),
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            padding: EdgeInsets.symmetric(horizontal: 80.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              SizedBox(height: 370.0),
+              CircularProgressIndicator(
+                // backgroundColor: AppColors.white,
+                strokeWidth: 7,
+                valueColor: AlwaysStoppedAnimation<Color>(AppColors.cyan)
+              )
             ])
           )
-        ]),
-        Container(
-          width: screenWidth,
-          height: screenHeight,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 370.00),
-              CircularProgressIndicator(
-                strokeWidth: 6,
-                valueColor: AlwaysStoppedAnimation<Color>(AppColors.cyan),
-              )
-          ])
-        )
-      ])
+        ])
+      )
     );
   }
 }
